914ab583ccffd444d6131ef41e168e8cead80a75
Arya Karimi <devaryakrm@gmail.com>
2023-02-09 16:14:09 +0300

    Nav added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <h1>Babbaza Luxury Logo</h1>
+        <ul className="nav-menu">
+          <li>
+            <Link to={"home"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"gallery"}>Gallery</Link>
+          </li>
+          <li>
+            <Link to={"contact"}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;

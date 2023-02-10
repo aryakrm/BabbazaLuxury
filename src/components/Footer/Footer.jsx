8865ab1d18c82aa8e584c8292b9704eba2713dec
Arya Karimi <devaryakrm@gmail.com>
2023-02-10 14:48:21 +0300

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Footer.scss";
-
+import { BsInstagram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { Link } from "react-router-dom";
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <img className="logo" src="/babbazaLogo.png" alt="babbazaLogo" />
+      <p>info@babbaza.com</p>
+      <div className="footer-icons">
+        <a href="https://www.instagram.com/luxebabazadesign/" target="_blank">
+          <BsInstagram />
+        </a>
+        <a href="https://wa.me/905428403134" target="_blank">
+          <BsWhatsapp />
+        </a>
+      </div>
+      <p>© 2023 Babbaza Luxury. All rights reserved.</p>
+    </footer>
+  );
 }
 
 export default Footer;
